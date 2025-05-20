44cbe56a0f545b6d69911767c4dd2f44e20ab154
vasapolrittideah <vasapol.rittideah@outlook.com>
2025-05-21 00:55:19 +0700

    feat(core): add custom error handling and response structures

---
+++ shared/domain/apperror/apperror.go
@@ -0,0 +1,27 @@
+package apperror
+
+import (
+	"fmt"
+
+	"google.golang.org/grpc/codes"
+)
+
+type Error struct {
+	Code codes.Code
+	Err  error
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("[%s] %v", e.Code.String(), e.Err)
+}
+
+func (e *Error) Unwrap() error {
+	return e.Err
+}
+
+func New(code codes.Code, err error) *Error {
+	return &Error{
+		Code: code,
+		Err:  err,
+	}
+}
