78b4a8ec2ede7fcd8c4f3f3c86d505a89ee7de3c
vasapolrittideah <vasapol.rittideah@outlook.com>
2025-05-21 15:28:32 +0700

    refactor(core): use caarlos0/env for parsing env into structs

--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/vasapolrittideah/money-tracker-api
 go 1.24.3
 
 require (
+	github.com/caarlos0/env/v11 v11.3.1
 	github.com/charmbracelet/lipgloss v1.1.0
 	github.com/charmbracelet/log v0.4.2
 	github.com/gofiber/fiber/v2 v2.52.6
 	github.com/google/uuid v1.6.0
-	github.com/spf13/viper v1.20.1
 	google.golang.org/grpc v1.72.1
 	google.golang.org/protobuf v1.36.5
 	gorm.io/driver/postgres v1.5.11
@@ -21,9 +21,7 @@ require (
 	github.com/charmbracelet/x/ansi v0.8.0 // indirect
 	github.com/charmbracelet/x/cellbuf v0.0.13-0.20250311204145-2c3ea96c31dd // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
-	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
-	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
@@ -36,20 +34,11 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/muesli/termenv v0.16.0 // indirect
-	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/sagikazarmark/locafero v0.7.0 // indirect
-	github.com/sourcegraph/conc v0.3.0 // indirect
-	github.com/spf13/afero v1.12.0 // indirect
-	github.com/spf13/cast v1.7.1 // indirect
-	github.com/spf13/pflag v1.0.6 // indirect
-	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/net v0.35.0 // indirect
@@ -57,5 +46,4 @@ require (
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- shared/config/config.go
+++ shared/config/config.go
@@ -3,44 +3,42 @@ package config
 import (
 	"time"
 
-	"github.com/spf13/viper"
-	"github.com/vasapolrittideah/money-tracker-api/shared/logger"
+	"github.com/caarlos0/env/v11"
 )
 
 type JwtConfig struct {
-	AccessTokenSecretKey  string        `mapstructure:"ACCESS_TOKEN_SECRET_KEY"`
-	AccessTokenExpiresIn  time.Duration `mapstructure:"ACCESS_TOKEN_EXPIRES_IN"`
-	RefreshTokenSecretKey string        `mapstructure:"REFRESH_TOKEN_SECRET_KEY"`
-	RefreshTokenExpiresIn time.Duration `mapstructure:"REFRESH_TOKEN_EXPIRES_IN"`
+	AccessTokenSecretKey  string        `env:"ACCESS_TOKEN_SECRET_KEY"`
+	AccessTokenExpiresIn  time.Duration `env:"ACCESS_TOKEN_EXPIRES_IN"`
+	RefreshTokenSecretKey string        `env:"REFRESH_TOKEN_SECRET_KEY"`
+	RefreshTokenExpiresIn time.Duration `env:"REFRESH_TOKEN_EXPIRES_IN"`
 }
 
 type DatabaseConfig struct {
-	Host     string `mapstructure:"POSTGRES_HOST"`
-	Port     string `mapstructure:"POSTGRES_PORT"`
-	User     string `mapstructure:"POSTGRES_USER"`
-	Password string `mapstructure:"POSTGRES_PASSWORD"`
-	Name     string `mapstructure:"POSTGRES_DB"`
+	Host     string `env:"POSTGRES_HOST"`
+	Port     string `env:"POSTGRES_PORT"`
+	User     string `env:"POSTGRES_USER"`
+	Password string `env:"POSTGRES_PASSWORD"`
+	Name     string `env:"POSTGRES_DB"`
 }
 
 type ServerConfig struct {
-	AuthHttpPort string `mapstructure:"AUTH_SERVICE_HTTP_PORT"`
-	UserHttpPort string `mapstructure:"USER_SERVICE_HTTP_PORT"`
-	UserGrpcPort string `mapstructure:"USER_SERVICE_GRPC_PORT"`
+	AuthHttpPort string `env:"AUTH_SERVICE_HTTP_PORT"`
+	UserHttpPort string `env:"USER_SERVICE_HTTP_PORT"`
+	UserGrpcPort string `env:"USER_SERVICE_GRPC_PORT"`
 }
 
 type Config struct {
-	Environment string         `mapstructure:"ENVIRONMENT"`
-	Jwt         JwtConfig      `mapstructure:",squash"`
-	Server      ServerConfig   `mapstructure:",squash"`
-	Database    DatabaseConfig `mapstructure:",squash"`
+	Environment string `env:"ENVIRONMENT"`
+	Jwt         JwtConfig
+	Server      ServerConfig
+	Database    DatabaseConfig
 }
 
-func Load() (config *Config, err error) {
-	viper.AutomaticEnv()
-
-	if err := viper.Unmarshal(&config); err != nil {
-		logger.Fatal("CORE", "unable to decode into struct: %v", err)
+func Load() (*Config, error) {
+	var config Config
+	if err := env.Parse(&config); err != nil {
+		return nil, err
 	}
 
-	return
+	return &config, nil
 }
