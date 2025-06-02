d170fd0dac5122b9beaa3c46b647c20460899fc3
vasapolrittideah <vasapol.rittideah@outlook.com>
2025-06-02 19:15:23 +0700

    Revert "refactor(auth): split AuthService into smaller functions"
    This reverts commit eadf0f08e09259d1c514d7ae3cf67a3af59198d1.

--- services/auth/service/service.go
+++ services/auth/service/service.go
@@ -40,18 +40,30 @@ func (s *authService) SignUp(req *model.SignUpRequest) (*model.SignUpResponse, *
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	hashedPassword, err := s.hashUserPassword(req.Password)
+	hashedPassword, err := passwordutil.HashPassword(req.Password)
 	if err != nil {
 		return nil, err
 	}
 
-	userProto, err := s.createNewUser(ctx, req, hashedPassword)
-	if err != nil {
-		return nil, err
+	newUser := entity.User{
+		FullName:       req.FullName,
+		Email:          req.Email,
+		HashedPassword: hashedPassword,
+	}
+
+	res, grpcErr := s.userClient.CreateUser(ctx, &userpb.CreateUserRequest{
+		FullName:       newUser.FullName,
+		Email:          newUser.Email,
+		HashedPassword: newUser.HashedPassword,
+	})
+	if grpcErr != nil {
+		st := status.Convert(grpcErr)
+		logger.Error("AUTH", "%s", st.Err())
+		return nil, apperror.New(st.Code(), st.Err())
 	}
 
 	return &model.SignUpResponse{
-		User: mapper.MapUserProtoToEntity(userProto),
+		User: mapper.MapUserProtoToEntity(res.User),
 	}, nil
 }
 
@@ -59,79 +71,20 @@ func (s *authService) SignIn(req *model.SignInRequest) (*model.SignInResponse, *
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	user, err := s.getUserByEmail(ctx, req.Email)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := s.verifyUserPassword(user.HashedPassword, req.Password); err != nil {
-		return nil, err
-	}
-
-	accessToken, refreshToken, err := s.generateJwtTokens(user.Id)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := s.saveRefreshToken(ctx, user, refreshToken); err != nil {
-		return nil, err
-	}
-
-	return &model.SignInResponse{
-		Jwt: &entity.Jwt{
-			AccessToken:  accessToken,
-			RefreshToken: refreshToken,
-		},
-	}, nil
-}
-
-func (s *authService) hashUserPassword(password string) (string, *apperror.Error) {
-	hashedPassword, err := passwordutil.HashPassword(password)
-	if err != nil {
-		return "", apperror.New(codes.Internal, fmt.Errorf("failed to hash password: %v", err.Error()))
-	}
-	return hashedPassword, nil
-}
-
-func (s *authService) createNewUser(
-	ctx context.Context,
-	req *model.SignUpRequest,
-	hashedPassword string,
-) (*userpb.User, *apperror.Error) {
-	res, grpcErr := s.userClient.CreateUser(ctx, &userpb.CreateUserRequest{
-		FullName:       req.FullName,
-		Email:          req.Email,
-		HashedPassword: hashedPassword,
-	})
-	if grpcErr != nil {
-		st := status.Convert(grpcErr)
-		logger.Error("AUTH", "%s", st.Err())
-		return nil, apperror.New(st.Code(), st.Err())
-	}
-	return res.User, nil
-}
-
-func (s *authService) getUserByEmail(ctx context.Context, email string) (*userpb.User, *apperror.Error) {
 	res, grpcErr := s.userClient.GetUserByEmail(ctx, &userpb.GetUserByEmailRequest{
-		Email: email,
+		Email: req.Email,
 	})
 	if grpcErr != nil {
 		st := status.Convert(grpcErr)
 		return nil, apperror.New(st.Code(), st.Err())
 	}
-	return res.User, nil
-}
 
-func (s *authService) verifyUserPassword(hashedPassword, inputPassword string) *apperror.Error {
-	ok, err := passwordutil.VerifyPassword(hashedPassword, inputPassword)
-	if err != nil || !ok {
-		return apperror.New(codes.Unauthenticated, fmt.Errorf("password is incorrect"))
+	user := res.User
+	userIdUuid := uuid.MustParse(user.Id)
+
+	if ok, err := passwordutil.VerifyPassword(user.HashedPassword, req.Password); err != nil || !ok {
+		return nil, apperror.New(codes.Unauthenticated, fmt.Errorf("password is incorrect"))
 	}
-	return nil
-}
-
-func (s *authService) generateJwtTokens(userId string) (string, string, *apperror.Error) {
-	userIdUuid := uuid.MustParse(userId)
 
 	accessToken, err := jwtutil.GenerateJwt(
 		s.cfg.Jwt.AccessTokenExpiresIn,
@@ -139,7 +92,7 @@ func (s *authService) generateJwtTokens(userId string) (string, string, *apperro
 		userIdUuid,
 	)
 	if err != nil {
-		return "", "", apperror.New(codes.Internal, fmt.Errorf("failed to generate access token: %v", err.Error()))
+		return nil, apperror.New(codes.Internal, fmt.Errorf("failed to generate access token: %v", err.Error()))
 	}
 
 	refreshToken, err := jwtutil.GenerateJwt(
@@ -148,29 +101,31 @@ func (s *authService) generateJwtTokens(userId string) (string, string, *apperro
 		userIdUuid,
 	)
 	if err != nil {
-		return "", "", apperror.New(codes.Internal, fmt.Errorf("failed to generate refresh token: %v", err.Error()))
+		return nil, apperror.New(codes.Internal, fmt.Errorf("failed to generate refresh token: %v", err.Error()))
 	}
 
-	return accessToken, refreshToken, nil
-}
-
-func (s *authService) saveRefreshToken(ctx context.Context, user *userpb.User, refreshToken string) *apperror.Error {
 	hashedRefreshToken, err := jwtutil.HashRefreshToken(refreshToken)
 	if err != nil {
-		return apperror.New(
+		return nil, apperror.New(
 			codes.Internal,
 			fmt.Errorf("failed to hash newly generated refresh token: %v", err.Error()),
 		)
 	}
 
 	user.HashedRefreshToken = hashedRefreshToken
-
 	if _, err = s.userClient.UpdateUser(ctx, &userpb.UpdateUserRequest{
 		User: user,
 	}); err != nil {
 		st := status.Convert(err)
-		return apperror.New(st.Code(), st.Err())
+		return nil, apperror.New(st.Code(), st.Err())
 	}
 
-	return nil
+	jwtRes := &model.SignInResponse{
+		Jwt: &entity.Jwt{
+			AccessToken:  accessToken,
+			RefreshToken: refreshToken,
+		},
+	}
+
+	return jwtRes, nil
 }
