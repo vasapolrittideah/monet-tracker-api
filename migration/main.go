7d176c96a797aeb9f798fd115812b0b4d1f0b541
vasapolrittideah <vasapol.rittideah@outlook.com>
2025-06-06 10:35:08 +0700

    refactor: move migration responsibility out of service

---
+++ migration/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	"github.com/charmbracelet/log"
+	"github.com/joho/godotenv"
+	"github.com/vasapolrittideah/money-tracker-api/shared/bootstrap"
+	"github.com/vasapolrittideah/money-tracker-api/shared/domain"
+)
+
+func main() {
+	// load .env file manually only for this file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	os.Setenv("POSTGRES_HOST", "localhost")
+
+	app := bootstrap.App()
+	defer app.Close()
+
+	if err := app.DB.AutoMigrate(
+		&domain.User{},
+	); err != nil {
+		log.Fatal("failed to migrate database: %v", err)
+	}
+
+	log.Info("🎉 database migrated successfully")
+}
