cfce356e17b80034c2e2f42cc99d950a1a1e75e4
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2025-06-17 21:54:23 +0700

    refactor(core): extract consul service registration helper

--- shared/consul/consul.go
+++ shared/consul/consul.go
@@ -2,6 +2,7 @@ package consul
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/charmbracelet/log"
@@ -10,11 +11,71 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+// ConnectGRPCServiceWithConsul connects to the given list of gRPC services registered in Consul
+func ConnectGRPCServiceWithConsul(
+	consulHost string,
+	consulPort string,
+	serviceNames []string,
+) (map[string]*grpc.ClientConn, error) {
+	address := fmt.Sprintf("%v:%v", consulHost, consulPort)
+	consulClient, err := newConsulClient(address)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create consul client: %w", err)
+	}
+
+	log.Infof("connecting to consul at %s", address)
+
+	return consulClient.createClientConnections(serviceNames)
+}
+
+// RegisterGRPCServiceWithConsul registers the gRPC service in Consul and deregisters on context cancellation
+func RegisterGRPCServiceWithConsul(
+	consulHost string,
+	consulPort string,
+	serviceID string,
+	serviceName string,
+	serviceAddress string,
+	servicePort string,
+	checkInterval time.Duration,
+	deregisterAfter time.Duration,
+) (func() error, error) {
+	address := fmt.Sprintf("%v:%v", consulHost, consulPort)
+	consulClient, err := newConsulClient(address)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create consul client: %w", err)
+	}
+
+	if err := consulClient.registerService(
+		serviceID,
+		serviceName,
+		serviceAddress,
+		servicePort,
+		checkInterval,
+		deregisterAfter,
+	); err != nil {
+		return nil, fmt.Errorf("failed to register service: %w", err)
+	}
+
+	log.Infof("🎉 %s registered successfully with consul", serviceName)
+
+	deregisterFunc := func() error {
+		if err := consulClient.deregisterService(serviceID); err != nil {
+			return fmt.Errorf("failed to deregister %s: %v", serviceName, err)
+		}
+
+		log.Infof("%s deregistered successfully", serviceName)
+
+		return nil
+	}
+
+	return deregisterFunc, nil
+}
+
 type ConsulClient struct {
 	client *consulapi.Client
 }
 
-func NewConsulClient(address string) (*ConsulClient, error) {
+func newConsulClient(address string) (*ConsulClient, error) {
 	config := consulapi.DefaultConfig()
 	config.Address = address
 
@@ -26,7 +87,7 @@ func NewConsulClient(address string) (*ConsulClient, error) {
 	return &ConsulClient{client: client}, nil
 }
 
-func (c *ConsulClient) CreateGRPCServiceConnections(serviceNames []string) (map[string]*grpc.ClientConn, error) {
+func (c *ConsulClient) createClientConnections(serviceNames []string) (map[string]*grpc.ClientConn, error) {
 	conns := make(map[string]*grpc.ClientConn)
 
 	for _, name := range serviceNames {
@@ -47,58 +108,42 @@ func (c *ConsulClient) CreateGRPCServiceConnections(serviceNames []string) (map[
 	return conns, nil
 }
 
-func (c *ConsulClient) RegisterGRPCService(
+func (c *ConsulClient) registerService(
 	serviceID,
 	serviceName,
 	serviceAddress string,
-	servicePort int,
+	servicePort string,
 	checkInterval time.Duration,
 	deregisterAfter time.Duration,
 ) error {
+	servicePortInt, _ := strconv.Atoi(servicePort)
 	reg := &consulapi.AgentServiceRegistration{
 		ID:      serviceID,
 		Name:    serviceName,
 		Address: serviceAddress,
-		Port:    servicePort,
+		Port:    servicePortInt,
 		Check: &consulapi.AgentServiceCheck{
-			GRPC:                           fmt.Sprintf("%s:%d", serviceAddress, servicePort),
+			GRPC:                           fmt.Sprintf("%s:%d", serviceAddress, servicePortInt),
 			Interval:                       checkInterval.String(),
 			Timeout:                        "1s",
 			DeregisterCriticalServiceAfter: deregisterAfter.String(),
 		},
 	}
 
-	if err := c.client.Agent().ServiceRegister(reg); err != nil {
-		return fmt.Errorf("failed to register service %v: %v", serviceName, err)
-	}
-
-	return nil
+	return c.client.Agent().ServiceRegister(reg)
 }
 
-func (c *ConsulClient) DeregisterService(serviceID string) error {
-	if err := c.client.Agent().ServiceDeregister(serviceID); err != nil {
-		return fmt.Errorf("failed to deregister service %v: %v", serviceID, err)
-	}
-
-	return nil
+func (c *ConsulClient) deregisterService(serviceID string) error {
+	return c.client.Agent().ServiceDeregister(serviceID)
 }
 
-func (c *ConsulClient) discoverServices(serviceName string) ([]*consulapi.ServiceEntry, error) {
+func (c *ConsulClient) connectToService(serviceName string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 	services, _, err := c.client.Health().Service(serviceName, "", true, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to discover services for %s: %w", serviceName, err)
 	}
 	if len(services) == 0 {
 		return nil, fmt.Errorf("no healthy instances found for service %s", serviceName)
-	}
-
-	return services, nil
-}
-
-func (c *ConsulClient) connectToService(serviceName string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
-	services, err := c.discoverServices(serviceName)
-	if err != nil {
-		return nil, err
 	}
 
 	svc := services[0]
