a3410d47c3a9e2ec12784d45a9e402b970c6107d
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2025-06-28 18:55:04 +0700

    refactor(core): add Claims struct for jwt token generation

--- shared/utils/tokenutil/tokenutil.go
+++ shared/utils/tokenutil/tokenutil.go
@@ -1,53 +1,56 @@
 package tokenutil
 
 import (
-	"fmt"
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 )
 
-func GenerateToken(ttl time.Duration, secretKey string, userID uint64) (string, error) {
-	now := time.Now()
-	claims := jwt.MapClaims{
-		"sub": userID,
-		"exp": now.Add(ttl).Unix(),
-		"iat": now.Unix(),
-	}
-
-	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(secretKey))
-	if err != nil {
-		return "", fmt.Errorf("failed to generate token: %v", err)
-	}
-
-	return token, nil
+type Claims struct {
+	UserID    uint64 `json:"uid"`
+	SessionID uint64 `json:"sid"`
+	jwt.RegisteredClaims
 }
 
-func ValidateToken(token string, secretKey string) (*jwt.Token, error) {
-	parsed, err := jwt.Parse(token, func(t *jwt.Token) (any, error) {
+// GenerateToken creates a JWT with custom claims
+func GenerateToken(ttl time.Duration, secretKey string, userID, sessionID uint64) (string, error) {
+	now := time.Now()
+	claims := &Claims{
+		UserID:    userID,
+		SessionID: sessionID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(now.Add(ttl)),
+			IssuedAt:  jwt.NewNumericDate(now),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+// ValidateToken parses and validates a JWT string
+func ValidateToken(tokenString string, secretKey string) (*Claims, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (any, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+			return nil, errors.New("unexpected signing method")
 		}
 
 		return []byte(secretKey), nil
 	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse token: %v", err.Error())
+		return nil, err
 	}
 
-	return parsed, nil
-}
-
-func ParseToken(tokenString string, secretKey string) (*jwt.MapClaims, error) {
-	token, err := ValidateToken(tokenString, secretKey)
-	if err != nil {
-		return nil, fmt.Errorf("failed to validate token: %v", err.Error())
+	if !token.Valid {
+		return nil, errors.New("invalid token")
 	}
 
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if !ok || !token.Valid {
-		return nil, fmt.Errorf("invalid token")
-	}
-
-	return &claims, nil
+	return claims, nil
 }
